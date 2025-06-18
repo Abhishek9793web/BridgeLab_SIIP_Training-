2a8ca72a47fcc33c9c7f66ebcbacb71072e40e86
Abhishek Goswami <113699791+Abhishek9793web@users.noreply.github.com>
2025-06-18 12:41:22 +0530

    [Abhishek goswami]Added String for Day5

--- String/src/Q_3.java
+++ String/src/Q_3.java
@@ -1,4 +1,38 @@
-package PACKAGE_NAME;
+import java.sql.SQLOutput;
+import java.util.Scanner;
 
 public class Q_3 {
+    public static void handling(String[] names) {
+
+        try {
+            System.out.println("Trying to access index 10 (out of bounds): " + names[10]);
+        } catch (ArrayIndexOutOfBoundsException e) {
+            System.out.println("Caught ArrayIndexOutOfBoundsException: " + e.getMessage());
+        } catch (RuntimeException e) {
+            System.out.println("Caught a RuntimeException: " + e.getMessage());
+        }
+
+    }
+    public static void generateException(String[] names){
+        System.out.println("Accessing index out of "+names[10]);
+    }
+
+    public static void main(String[] args) {
+        Scanner sc= new Scanner(System.in);
+
+        String[] names=new String[5];
+
+        System.out.println("enter  5 name");
+        for (int i = 0; i < names.length; i++) {
+            names[i]= sc.nextLine();
+        }
+        System.out.println("generating Exception");
+        try {
+           generateException(names);
+        } catch (ArrayIndexOutOfBoundsException e) {
+            System.out.println("Caught ArrayIndexOutOfBoundsException: " + e.getMessage());
+        }
+        System.out.println("Handling Exception");
+        handling(names);
+    }
 }
--- String/src/Q_4.java
+++ String/src/Q_4.java
@@ -1,4 +1,48 @@
-package PACKAGE_NAME;
+import java.util.Scanner;
 
 public class Q_4 {
+    public static String Tolowercase(String text){
+
+
+        StringBuilder result=new StringBuilder();
+        for (int i = 0; i < text.length(); i++) {
+            char ch=text.charAt(i);
+
+            if (ch >= 'A' && ch <= 'Z') {
+                ch = (char)(ch + 32); // Convert to uppercase
+            }
+            result.append(ch);
+        }
+        return result.toString();
+
+    }
+    public static boolean compstring(String s1,String s2){
+        if(s1.length() !=s2.length()){
+            return false;
+        }
+        for (int i = 0; i < s1.length(); i++) {
+            if(s1.charAt(i)!=s2.charAt(i)) {
+                return false;
+            }
+
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+
+        System.out.println("enter the text: ");
+        String input= sc.nextLine();
+
+        String munualLower=Tolowercase(input);
+        String buildin=input.toLowerCase();
+
+        boolean issame=compstring(munualLower,buildin);
+
+        System.out.println("Munual Upper: "+munualLower);
+        System.out.println("Build in : "+buildin);
+        System.out.println("Is same:  "+issame);
+
+    }
 }
